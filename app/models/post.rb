1e82dc8b9acf153eba20919575ac9b736e6ece1f
James Benjamin <james.abenj@gmail.com>
2020-05-27 20:32:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, inclusion: %w{"Won't Believe" "Secret" "Top[number]" "Guess"}
   validates :content, length: { minimum: 250}
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: %w{Fiction Non-Fiction}
