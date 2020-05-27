2dbfcc80a684a34d67bef1766ea2a43c45171397
James Benjamin <james.abenj@gmail.com>
2020-05-27 19:45:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { min: 250}
-  validates :summery, length: { max: 250}
+  validates :content, length: { minimum: 250}
+  validates :summery, length: { maximum: 250}
 end
